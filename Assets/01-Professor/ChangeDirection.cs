0942ae9548255779c1c10a5846e18458ff3a5b09
ShovelCrow <ksvasquez02@gmail.com>
2026-03-25 12:50:50 -0400

    lab

--- Assets/01-Professor/ChangeDirection.cs
+++ Assets/01-Professor/ChangeDirection.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ChangeDirection : MonoBehaviour
 {
+    private Animator myAnimator;
+    private float horizDir;
+    private float vertDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnimator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //read from the keyboard
+        horizDir = Input.GetAxis("Horizontal");
+        vertDir = Input.GetAxis("Vertical");
+
+        // use the input to send a message to the animator
+        if (horizDir < 0)
+        {
+            myAnimator.SetInteger("direction", 3);
+        }
+        else if (horizDir > 0)
+        {
+            myAnimator.SetInteger("direction", 1);
+        }
+        else if (vertDir > 0)
+        {
+            myAnimator.SetInteger("direction", 0);
+        }
+        else if (vertDir < 0)
+        {
+            myAnimator.SetInteger("direction", 2);
+        }
     }
 }
